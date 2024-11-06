786f8ee0d1e8d816744baabc809f7b17c94c3627
sumirpv <sumirpv@gmail.com>
2024-11-05 22:37:38 -0800

    Updates

---
+++ precache-manifest.80e8e60c8aaac2683194e40f7643665d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b232e23757c64e3ec839d2f3fd13d4",
+    "url": "/interview-app/index.html"
+  },
+  {
+    "revision": "f7dcec719f38c3b15212",
+    "url": "/interview-app/static/js/2.f1156afc.chunk.js"
+  },
+  {
+    "revision": "4080914dd7b277cd774e",
+    "url": "/interview-app/static/js/3.bdbac99b.chunk.js"
+  },
+  {
+    "revision": "e7efbb466b491c35e22d",
+    "url": "/interview-app/static/js/main.a36c118f.chunk.js"
+  },
+  {
+    "revision": "eb8bc94a2929ed597aef",
+    "url": "/interview-app/static/js/runtime~main.ed7abcde.js"
+  }
+]);
